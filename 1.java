2ac1c456744e9377dcf98cfe3f87500a1a8563bf
Mark Wong <markwong50523@gmail.com>
2019-11-03 17:54:37 -0600

    commit again

--- 1.java
+++ 1.java
@@ -6,8 +6,9 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        // System.out.println("Hello, World"); 
         System.out.println("Hello, World"); 
         System.out.println("new added lines"); 
+        System.out.println("new second added lines"); 
     } 
 } 
